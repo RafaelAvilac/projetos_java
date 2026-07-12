e61cfc1974e6c7d3c291c538f60995da6f0b7ee5
RafaelAvilac <rafael_avilac@yahoo.com.br>
2026-07-12 15:47:23 -0300

    Update Exercicio_14.java

--- projeto_java/src/Exercicio_14.java
+++ projeto_java/src/Exercicio_14.java
@@ -2,16 +2,13 @@ import java.util.Scanner;
 public class Exercicio_14{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n;
-        System .out.println("Digite um numero: ");
-        n = sc.nextInt();
-
-        int soma = n;
-
-        while(n > 0){
+        System.out.println("Digite um numero: ");
+        int n = sc.nextInt();
+        int soma = 0;
+        while(n != 0){
+            soma += n;
             System.out.println("Digite um numero: ");
             n = sc.nextInt();
-            soma += n;
         }
         System.out.println("Soma total: " + soma);
         sc.close();
